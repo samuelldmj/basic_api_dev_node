9da74b65d285e45b457262a8cd1b7e064b95c9a5
samuelldmj <samuelldmj5@gmail.com>
2025-02-25 13:50:57 +0100

    select a specific column to be rendered to the client

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,16 +1,15 @@
 const Post = require('../models/post');
 
-const getPostsController =  (req, res) => {
-    res.json({
-        post : [
-            {
-                title : "This is a first post"
-            },
-            {
-                title : "This is a second post" 
-            }
-        ]
-    });
+const getPostsController = async  (req, res) => {
+    const posts = await Post.find().select("_id title body");
+    try{
+       return res.status(200).json({posts: posts})
+    }
+    catch(err){
+        return res.status(400).json({
+            error: err,
+        });
+    }
 };
 
 
@@ -19,7 +18,7 @@ const createPostController = async (req, res) => {
 
     try {
         const result = await postModel.save();
-        return res.status(200).json({
+        return res.status(201).json({
             data: result
         });
     } catch (err) {
